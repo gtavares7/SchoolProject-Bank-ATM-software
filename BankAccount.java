68fd63e04b33b1cf3beba4b74e5f72ae85c91424
gtavares7 <gtavares.linux@gmail.com>
2022-04-15 10:07:57 -0400

    added private classes and constructor

--- BankAccount.java
+++ BankAccount.java
@@ -1,5 +1,22 @@
+
+import java.util.*;
 public class BankAccount {
 
+    private static final double annualInterestRate = 4.5;
+    private int nextAccountNb = 300;
+    private final int accountNb;
+    private String firstName;
+    private String lastName;
+    private double balance;
+    private final Date dateCreated;
 
+    BankAccount (String firstName, String lastName, double balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+        accountNb = nextAccountNb;
+        dateCreated = new Date();
+        this.nextAccountNb++;
+    }
 
 }
