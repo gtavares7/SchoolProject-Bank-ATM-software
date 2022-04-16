f8f29aa7a741fffc381cb2a02bd8508ede02b26c
Gabriel Tavares <gtavares.linux@gmail.com>
2022-04-16 09:54:23 -0400

    added method for json file reading

--- BankAccount.java
+++ BankAccount.java
@@ -2,6 +2,8 @@ import org.json.simple.JSONObject;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.Reader;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -84,7 +86,7 @@ public class BankAccount {
     }
 
     // method to read/write to .json file
-    public static void json() {
+    public static void writeJson() {
 
         try {
 
@@ -103,14 +105,51 @@ public class BankAccount {
 
             // create accountType object
             JSONObject accountType = new JSONObject();
-            customer.put("", "");
+            accountType.put("", "");
 
+            // add accountType to customer
+            customer.put("accountType", accountType);
+
+            // write JSON to file
+            Jsoner.serialize(customer, writer);
+
+            // close the writer
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readJson() {
+        try {
+
+            // create a reader
+            Reader reader = Files.newBufferedReader(Paths.get("account_details.json"));
+
+            // create parser
+            JSONObject parser = (JSONObject) Jsoner.deserialize(reader);
+
+            // read account details
+            BigDecimal accountNb = (BigDecimal) parser.get("accountNb");
+            String firstName = (String) parser.get("firstName");
+            String lastName = (String) parser.get("lastName");
+            String username = (String) parser.get("username");
+            String password = (String) parser.get("password");
+            BigDecimal balance = (BigDecimal) parser.get("balance");
+            String dateCreated = (String) parser.get("dateCreated");
+
+            // read accountType
+            Map<Object, Object> accountType = (Map<Object, Object>) parser.get("accountType");
+            accountType.forEach((key, value) -> System.out.println(key + ":" + value));
+
+            // close reader
+            reader.close();
 
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
 }
