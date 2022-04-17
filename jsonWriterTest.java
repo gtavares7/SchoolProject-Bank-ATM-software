4b0a24dcf98b3da036ccaed60f0facf8fb38742d
Gabriel Tavares <gtavares.linux@gmail.com>
2022-04-17 12:54:00 -0400

    imported packages for test file

--- jsonWriterTest.java
+++ jsonWriterTest.java
@@ -1,6 +1,16 @@
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class jsonWriterTest {
 
+    public static void writeJson() {
+
+        
+
+    }
 
 }
