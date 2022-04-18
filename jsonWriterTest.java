b5bb8fc8db7cb21cccb9ecef465a145f379bee16
Gabriel Tavares <gtavares.linux@gmail.com>
2022-04-18 12:08:29 -0400

    Update jsonWriterTest.java

--- jsonWriterTest.java
+++ jsonWriterTest.java
@@ -17,7 +17,7 @@ public class jsonWriterTest {
         accountDetails.put("username", "Tavares7");
         accountDetails.put("password", "abcd1234");
         accountDetails.put("balance", 1000);
-        accountDetails.put("dateCreated", "17/04/2022");
+        accountDetails.put("dateCreated", "17-04-2022");
 
         // create accountType object within accountDetails
         // JSONObject accountType = new JSONObject();
@@ -31,41 +31,52 @@ public class jsonWriterTest {
         JSONObject accountObject = new JSONObject();
         accountObject.put("account", accountNbObject);
 
-
         // create second account object
-        /* JSONObject accountDetails2 = new JSONObject();
-        accountDetails2.put("accountNb", 1);
-        accountDetails2.put("firstName", "");
-        accountDetails2.put("lastName", "");
-        accountDetails2.put("username", "");
-        accountDetails2.put("password", "");
-        accountDetails2.put("balance", 1);
-        accountDetails2.put("dateCreated", "");
+        JSONObject accountDetails2 = new JSONObject();
+        // accountDetails2.put("accountNb", 1);
+        accountDetails2.put("firstName", "Eduardo");
+        accountDetails2.put("lastName", "Mazzonna");
+        accountDetails2.put("username", "eddy");
+        accountDetails2.put("password", "abcd1234");
+        accountDetails2.put("balance", 1000);
+        accountDetails2.put("dateCreated", "17-04-2022");
+
+        JSONObject accountNbObject2 = new JSONObject();
+        accountNbObject.put("accountNb", accountDetails2);
 
         JSONObject accountObject2 = new JSONObject();
-        accountObject2.put("account", accountDetails2);
+        accountObject.put("account", accountNbObject2);
+
+        // JSONObject accountObject2 = new JSONObject();
+        // accountObject2.put("account", accountDetails2);
 
         // create third account object
         JSONObject accountDetails3 = new JSONObject();
-        accountDetails3.put("accountNb", 1);
-        accountDetails3.put("firstName", "");
-        accountDetails3.put("lastName", "");
-        accountDetails3.put("username", "");
-        accountDetails3.put("password", "");
-        accountDetails3.put("balance", 1);
-        accountDetails3.put("dateCreated", "");
+        // accountDetails3.put("accountNb", 1);
+        accountDetails3.put("firstName", "Vincent");
+        accountDetails3.put("lastName", "Mariani");
+        accountDetails3.put("username", "vince");
+        accountDetails3.put("password", "abcd1234");
+        accountDetails3.put("balance", 1000);
+        accountDetails3.put("dateCreated", "18-04-2022");
+
+        JSONObject accountNbObject3 = new JSONObject();
+        accountNbObject.put("accountNb", accountDetails3);
 
         JSONObject accountObject3 = new JSONObject();
-        accountObject3.put("account", accountDetails3); */
+        accountObject.put("account", accountNbObject3);
+
+        // JSONObject accountObject3 = new JSONObject();
+        // accountObject3.put("account", accountDetails3);
 
         // add accounts to list
         JSONArray accountList = new JSONArray();
         accountList.add(accountObject);
-        // accountList.add(accountObject2);
-        // accountList.add(accountObject3);
+        accountList.add(accountObject2);
+        accountList.add(accountObject3);
 
         // write JSON file
-        try (FileWriter file = new FileWriter("account_details_test2.json")) {
+        try (FileWriter file = new FileWriter("account_details_test3.json")) {
             file.write(accountList.toJSONString());
             file.flush();
 
